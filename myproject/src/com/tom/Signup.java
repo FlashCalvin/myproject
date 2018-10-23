ff0311fbfe9b8da639d3845274b2c364d31dcee5
user <user@es302n12>
2018-10-23 12:01:54 +0800

    yeah

--- myproject/src/com/tom/Signup.java
+++ myproject/src/com/tom/Signup.java
@@ -1,5 +1,31 @@
 package com.tom;
 
-public class Signup {
+import java.util.Scanner;
 
-}
+public class Signup {
+	public static void main(String[]args) {
+		System.out.println("Are you 18, y/n");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+	    boolean adult =line.toUpperCase().equals("Y");
+		if (adult) {
+			System.out.print("Your Age");
+			int Age = scanner.nextInt();
+			System.out.print("Your Name");
+			int Name = scanner.nextInt();
+			System.out.print("Your Nickname");
+			int Nickname = scanner.nextInt();
+			//System.out.println("Your Age 
+			
+		}   else {
+			System.out.println("Subnormal Child, Goodbye");
+		}
+	}
+		
+		
+
+	
+
+	     
+
+
