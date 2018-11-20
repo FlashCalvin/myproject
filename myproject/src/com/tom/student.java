66c336612ec6020c9781cc0b893cf0f09b42a0eb
user <user@es302n08>
2018-11-20 12:02:07 +0800

    ass

--- myproject/src/com/tom/student.java
+++ myproject/src/com/tom/student.java
@@ -1,9 +1,32 @@
 package com.tom;
 
-/*public class student {
+public class student{
 	String name;
-	int english;		
-    int math;
-    
-    public Student() {
-    }*/
+	int english;
+	int math;
+	
+//	public Student(String name , int english, int math) {
+//		this.name = name;
+//		this.math = english = english;
+//		this.math = math;
+//	
+//	}
+//	public void print(){
+//		System.out.print(name +"/t" + english + "/t" + math + "/t" + (english + math)/2 + "/t");
+//		if (getAverage() < 60){
+//			System.out.ptintln("Failed");
+//		}else{
+//			System.out.println("Pass");
+//		}
+//		public char getGrading( {
+//			char grading = 'F';
+//			int average = getAverage();
+			
+			
+//		}
+		
+	}
+	
+//}
+
+
